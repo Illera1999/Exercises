e4f989da2f2a18a165af427ef8156f6c0fc90eee
Illera1999 <illerarodriguez1999@gmail.com>
2024-01-06 00:20:00 +0000

    AddTwoNumbers in progress

---
+++ cpp/addTwoNum.cpp
@@ -0,0 +1,62 @@
+/*
+You are given two non-empty linked lists representing two non-negative integers. 
+The digits are stored in reverse order, and each of their nodes contains a single digit. 
+Add the two numbers and return the sum as a linked list.
+
+You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+
+Example 1:
+Input: l1 = [2,4,3], l2 = [5,6,4]
+Output: [7,0,8]
+Explanation: 342 + 465 = 807.
+
+Example 2:
+
+Input: l1 = [0], l2 = [0]
+Output: [0]
+
+Example 3:
+
+Input: l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9]
+Output: [8,9,9,9,0,0,0,1]
+*/
+
+#include <iostream>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        while (l1->next != nullptr){
+            addTwoNumbers(ListNode* l1->next, ListNode* l2->next);
+        }
+
+    }
+};
+
+int main(){
+    ListNode* node1 = new ListNode(1);
+    ListNode* node2 = new ListNode(2);
+    ListNode* node3 = new ListNode(3);
+
+    node1->next = node2;
+    node2->next = node3;
+
+    ListNode* node4 = new ListNode(1);
+    ListNode* node5 = new ListNode(2);
+    ListNode* node6 = new ListNode(3);
+
+    node4->next = node5;
+    node5->next = node6;
+
+    Solution solution;
+    ListNode* result = solution.addTwoNumbers(node1, node4);
+}
